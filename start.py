df6627b8e8606a8ca83fdff62d20d24038128f38
Peter Snyder <snyderp@gmail.com>
2014-09-28 18:06:19 -0500

    fix static file handling

--- start.py
+++ start.py
@@ -2,9 +2,11 @@ import config
 import tornado.web
 import webcommon.server
 import bitssignup.controllers
+import os
 
+root_dir = os.path.dirname(os.path.realpath(__file__))
 routes = [
-    (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": "static"}),
+    (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": os.path.join(root_dir, "static")}),
     (r"/", bitssignup.controllers.MainHandler)
 ]
 
