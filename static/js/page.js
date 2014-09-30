b68db3e23ad1f5bc1b9bb0a51d17faddee2e1155
Peter Snyder <snyderp@gmail.com>
2014-09-30 03:16:10 -0500

    -

--- static/js/page.js
+++ static/js/page.js
@@ -3,10 +3,5 @@ jQuery(function ($) {
     var $fieldset = $("fieldset"),
         $form = $("form", $fieldset);
 
-    $form.validate({
-         submitHandler: function (thisForm) {
-            thisForm.submit();
-            $fieldset.attr("disabled", "disabled");
-        }
-    });
+    $form.validate();
 });
