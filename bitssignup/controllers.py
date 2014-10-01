6346148080a4f2db99d35d70122603408d41e7c8
Peter Snyder <snyderp@gmail.com>
2014-10-01 12:38:36 -0500

    add Strict-Transport-Security header

--- bitssignup/controllers.py
+++ bitssignup/controllers.py
@@ -2,16 +2,28 @@ import tornado.web
 from tornado import gen
 import lepl.apps.rfc3696
 import datetime
+import config
 
 email_validator = lepl.apps.rfc3696.Email()
 
-class MainHandler(tornado.web.RequestHandler):
+class BaseHandler(tornado.web.RequestHandler):
+
+    def prepare(self):
+        # If we're not in debugging mode (ie we're in "production"),
+        # send down HTTP Strict Transport Security header to make sure
+        # we're only visited via https
+        if not config.debug:
+            self.set_header('Strict-Transport-Security',
+                            'max-age=16070400; includeSubDomains')
+        super(BaseHandler, self).prepare()
+
+class MainHandler(BaseHandler):
     TEMPLATE = "main.html"
 
     def get(self):
         self.render(MainHandler.TEMPLATE)
 
-class SignupHandler(tornado.web.RequestHandler):
+class SignupHandler(BaseHandler):
 
     TEMPLATE = "signup.html"
     VALID_DATE_LOCATIONS = (
